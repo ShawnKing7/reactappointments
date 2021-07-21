4b33a82c64da632cf4aaf960569e45e52b926447
Cuda572 <81455848+Cuda572@users.noreply.github.com>
2021-07-21 13:01:07 -0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { FaFileMedical } from "react-icons/fa"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mx-auto mt-3 font-thin">
+     <h1 className="text-5xl">
+       < FaFileMedical className="inline-block text-red-600 align-top" /> My Appointments</h1>
     </div>
   );
 }
 
 export default App;
+                                                                                                                                                                                                                                                                                                                                                                                      
